5407da80b002b16cbd7b584e6c3654f4cbbf9426
Vipul Kumar Singh <vipul24.9669@gmail.com>
2025-02-09 20:43:46 +0530

    15. Working With Core Components

--- 02-ReactNativeBasics-GoalsApp/RNCourse/App.js
+++ 02-ReactNativeBasics-GoalsApp/RNCourse/App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Another piece of Text!</Text>
+      </View>
+      <Text>Hello World!</Text>
+      <Button title='Tap Me!'/>
     </View>
   );
 }
