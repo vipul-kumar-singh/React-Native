ecd6471dad4186cd8e1a6ce952734cdca55cba0b
Vipul Kumar Singh <vipul24.9669@gmail.com>
2025-02-15 20:07:21 +0530

    26. Making Content Scrollable with ScrollView

--- 02-ReactNativeBasics-GoalsApp/RNCourse/App.js
+++ 02-ReactNativeBasics-GoalsApp/RNCourse/App.js
@@ -1,5 +1,12 @@
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+} from 'react-native';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -27,11 +34,13 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoadHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal) => (
-          <View key={goal} style={styles.goalItem}>
-            <Text style={styles.goalText}>{goal}</Text>
-          </View>
-        ))}
+        <ScrollView alwaysBounceVertical='false'>
+          {courseGoals.map((goal) => (
+            <View key={goal} style={styles.goalItem}>
+              <Text style={styles.goalText}>{goal}</Text>
+            </View>
+          ))}
+        </ScrollView>
       </View>
     </View>
   );
