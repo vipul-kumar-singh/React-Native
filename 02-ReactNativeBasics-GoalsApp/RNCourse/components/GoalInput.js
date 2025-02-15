e070940ad1f9505fadd372e6bf16295a3c709590
Vipul Kumar Singh <vipul24.9669@gmail.com>
2025-02-16 01:01:23 +0530

    35. Styling the Modal Overlay

--- 02-ReactNativeBasics-GoalsApp/RNCourse/components/GoalInput.js
+++ 02-ReactNativeBasics-GoalsApp/RNCourse/components/GoalInput.js
@@ -22,7 +22,14 @@ function GoalInput(props) {
           onChangeText={goalInputHandler}
           value={enteredGoalText}
         />
-        <Button title="Add Goal" onPress={addGoalHandler} />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Add Goal" onPress={addGoalHandler} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Cancel" />
+          </View>
+        </View>
       </View>
     </Modal>
   );
@@ -31,19 +38,26 @@ function GoalInput(props) {
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 24,
     borderBottomWidth: 1,
     borderBottomColor: '#cccccc',
+    padding: 16,
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '70%',
-    marginRight: 8,
+    width: '100%',
     padding: 8,
+  },
+  buttonContainer: {
+    marginTop: 16,
+    flexDirection: 'row',
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8,
   },
 });
 
