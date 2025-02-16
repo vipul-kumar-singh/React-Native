12c3f88a8c46b9d37d92b9d4f9c19b6df0736d37
Vipul Kumar Singh <vipul24.9669@gmail.com>
2025-02-16 15:40:33 +0530

    Debugging React native

---
+++ 03-Debugging/RNCourse/components/GoalItem.js
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+
+function GoalItem(props) {
+  return (
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: '#dddddd' }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
+        <Text style={styles.goalText}>{props.text}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+  },
+  pressedItem: {
+    opacit: 0.5,
+  },
+  goalText: {
+    color: 'white',
+    padding: 8,
+  },
+});
+
+export default GoalItem;
