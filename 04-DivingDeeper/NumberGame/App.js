7efc38a985d82f87f000fcce573b755de35caa52
Vipul Kumar Singh <vipul24.9669@gmail.com>
2025-02-16 22:33:06 +0530

    55. Coloring the Components & The Overall App

--- 04-DivingDeeper/NumberGame/App.js
+++ 04-DivingDeeper/NumberGame/App.js
@@ -1,10 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <View style={styles.rootScreen}>
+      <StartGameScreen />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+    backgroundColor: '#ddb52f',
+  },
+});
