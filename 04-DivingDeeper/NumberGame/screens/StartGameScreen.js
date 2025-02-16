cf6f6135470c0de38b7f988682ef13bc90e1de04
Vipul Kumar Singh <vipul24.9669@gmail.com>
2025-02-16 22:27:53 +0530

    54. Improving the Buttons

--- 04-DivingDeeper/NumberGame/screens/StartGameScreen.js
+++ 04-DivingDeeper/NumberGame/screens/StartGameScreen.js
@@ -11,14 +11,22 @@ function StartGameScreen() {
         autoCapitalize="none"
         autoCorrect={false}
       />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
     padding: 16,
     marginTop: 100,
     marginHorizontal: 24,
@@ -40,6 +48,12 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    flex: 1,
   },
 });
 
