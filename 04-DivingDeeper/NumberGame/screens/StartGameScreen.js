ff47c42e23e3ae3502d32c9d4f56ff8a28222273
Vipul Kumar Singh <vipul24.9669@gmail.com>
2025-02-16 20:47:37 +0530

    50. Styling for Android & iOS

--- 04-DivingDeeper/NumberGame/screens/StartGameScreen.js
+++ 04-DivingDeeper/NumberGame/screens/StartGameScreen.js
@@ -1,14 +1,29 @@
-import { TextInput, View } from 'react-native';
+import { TextInput, View, StyleSheet } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 
 function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    backgroundColor: '#72063c',
+    elevation: 4, // android-only property
+    shadowColor: 'black', // ios-only property
+    shadowOffset: { width: 0, height: 2 }, // ios-only property
+    shadowRadius: 6, // ios-only property
+    shadowOpacity: 0.25, // ios-only property
+  },
+});
 
 export default StartGameScreen;
