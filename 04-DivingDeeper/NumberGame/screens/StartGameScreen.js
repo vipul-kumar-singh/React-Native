57ad891cb10ea173d6b9d700737a6738e76a0b52
Vipul Kumar Singh <vipul24.9669@gmail.com>
2025-02-16 20:57:38 +0530

    51. Styling the Number-Input Element

--- 04-DivingDeeper/NumberGame/screens/StartGameScreen.js
+++ 04-DivingDeeper/NumberGame/screens/StartGameScreen.js
@@ -4,7 +4,7 @@ import PrimaryButton from '../components/PrimaryButton';
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput style={styles.numberInput} maxLength={2} />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -23,6 +23,17 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 }, // ios-only property
     shadowRadius: 6, // ios-only property
     shadowOpacity: 0.25, // ios-only property
+  },
+  numberInput: {
+    height: 60,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
 
